1b7bcfa608791ad01cdb2da7b688d7f983bbf423
djc4722 <31022447+djc4722@users.noreply.github.com>
2017-10-18 10:50:32 -0400

    1

---
+++ data.js
@@ -0,0 +1,323 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-entry-gate",
+      "name": "Entry Gate",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.7548081133446018,
+        "pitch": -0.0578657367509976,
+        "fov": 1.0
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6867693530161034,
+          "pitch": 0.0616168059902229,
+          "rotation": 0,
+          "target": "1-farmhouse-plaza"
+        },
+        {
+          "yaw": -2.3093964992885088,
+          "pitch": 0.015515913116070479,
+          "rotation": 0,
+          "target": "2-grove-and-fire-pit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-farmhouse-plaza",
+      "name": "Farmhouse Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.6292283691518996,
+        "pitch": 0.1289243785139078,
+        "fov": 1.0
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9979382894718363,
+          "pitch": 0.1126069685555926,
+          "rotation": 0,
+          "target": "0-entry-gate"
+        },
+        {
+          "yaw": -1.2390062184070771,
+          "pitch": 0.1139127689622974,
+          "rotation": 0,
+          "target": "2-grove-and-fire-pit"
+        },
+        {
+          "yaw": 0.21378337007401882,
+          "pitch": 0.13326430684548285,
+          "rotation": 0,
+          "target": "3-vestibule"
+        },
+        {
+          "yaw": -0.5793254796143206,
+          "pitch": 0.11090054007286199,
+          "rotation": 0,
+          "target": "4-multipurpose-room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-grove-and-fire-pit",
+      "name": "Grove and Fire Pit",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.1923462281644905,
+        "pitch": -0.004557749073811124,
+        "fov": 1.0
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.060888437609311,
+          "pitch": 0.004390739810290256,
+          "rotation": 0,
+          "target": "4-multipurpose-room"
+        },
+        {
+          "yaw": -1.3319163642867249,
+          "pitch": 0.02080257624292159,
+          "rotation": 0,
+          "target": "3-vestibule"
+        },
+        {
+          "yaw": -0.5575951801649808,
+          "pitch": 0.03882057065089484,
+          "rotation": 0,
+          "target": "1-farmhouse-plaza"
+        },
+        {
+          "yaw": 0.26558717806697807,
+          "pitch": -0.007230460267438232,
+          "rotation": 0,
+          "target": "0-entry-gate"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-vestibule",
+      "name": "Vestibule",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.239446709961401,
+        "pitch": -0.010658135389665091,
+        "fov": 1.0
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4299112863661367,
+          "pitch": 0.06037753437638216,
+          "rotation": 0,
+          "target": "1-farmhouse-plaza"
+        },
+        {
+          "yaw": 1.8519724435806806,
+          "pitch": 0.007219977526002452,
+          "rotation": 0,
+          "target": "0-entry-gate"
+        },
+        {
+          "yaw": -2.8995317213690317,
+          "pitch": 0.07696963935064005,
+          "rotation": 0,
+          "target": "4-multipurpose-room"
+        },
+        {
+          "yaw": -1.7825682931743057,
+          "pitch": 0.018979531153627605,
+          "rotation": 0,
+          "target": "5-overlook-terrace"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-multipurpose-room",
+      "name": "Multipurpose Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.9859050865950945,
+        "pitch": 0.053316039464652576,
+        "fov": 1.0
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8500649693396056,
+          "pitch": 0.06526937134011135,
+          "rotation": 0,
+          "target": "5-overlook-terrace"
+        },
+        {
+          "yaw": 1.9725379475238878,
+          "pitch": 0.048006806833278404,
+          "rotation": 0,
+          "target": "3-vestibule"
+        },
+        {
+          "yaw": 2.732934115637647,
+          "pitch": 0.050110919464357195,
+          "rotation": 0,
+          "target": "1-farmhouse-plaza"
+        },
+        {
+          "yaw": -2.4311427991288355,
+          "pitch": 0.05493258847609361,
+          "rotation": 0,
+          "target": "2-grove-and-fire-pit"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-overlook-terrace",
+      "name": "Overlook Terrace",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6731503221909012,
+        "pitch": 0.05941004975267816,
+        "fov": 1.0
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.957928096564233,
+          "pitch": 0.05471930847738804,
+          "rotation": 0,
+          "target": "3-vestibule"
+        },
+        {
+          "yaw": -1.2409584490350873,
+          "pitch": 0.032186609005854905,
+          "rotation": 0,
+          "target": "4-multipurpose-room"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "360 Panorama Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
